d31ef5c7da0fecfccc024caf6335facb06506ee7
Pedroh <pedrooohsss@gmail.com>
2024-06-05 21:17:59 -0300

    implementa fluxo da API

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
@@ -1,5 +1,6 @@
 package br.com.petz.clientepet.pet.application.api;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,14 @@ public class PetController implements PetAPI {
 		PetResponse pet = petService.criaPet(idCliente, petRequest);
 		log.info("[finaliza] PetController - postPet");
 		return pet;
+	}
+
+	@Override
+	public List<PetClienteListResponse> getPetsDoClienteComId(UUID idCliente) {
+		log.info("[inicia] PetController - getPetsDoClienteComId");
+		log.info("[idCliente] {}", idCliente);
+		log.info("[finish] PetController - getPetsDoClienteComId");
+		return null;
 	}
 
 }
